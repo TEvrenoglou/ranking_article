01fbfc8f51332b898d53f93d26f058c7b572319b
Theodoros Evrenoglou <43581879+TEvrenoglou@users.noreply.github.com>
2025-05-24 16:15:21 +0200

    Update example_diabetes.R

--- example_diabetes.R
+++ example_diabetes.R
@@ -12,13 +12,11 @@ library(R2jags)
 ### Code obtained from the GitHub page: https://github.com/esm-ispm-unibe-ch/alternativenma/tree/master/R
 
 source("./helpers/nma.krahn.output.R")
-
 source("./helpers/alternative_nma.R")
-
 source("./helpers/netmetaranks.R")
 
 
-### source code to get ranking based on the adjusted according to MCID P-scores
+### source code to get ranking based on the adjusted according to SWD P-scores
 ### Code obtained from the GitHub page: https://github.com/DimitrisMavridis/RankingNMA/blob/master/extendedP-scores
 
 source("./helpers/Pscores_function.R")
@@ -30,12 +28,6 @@ source("./helpers/Prepare_Single.R")
 source("./helpers/Prepare_function.R")
 source("./helpers/pscrs.R")
 source("./helpers/pscore_graph.R")
-
-
-### source code to get ranking based on the P(best) Bayesian approach
-source("./helpers/Bayesian_NMA_model.R")
-source("./helpers/get_results.R")
-
 
 ### get the 'diabetes' dataset from the mtrank package
 
@@ -52,10 +44,10 @@ p <- pairwise(data = diabetes,
               sm = "OR"
 )
 
-## fit a NMA model
+# fit a NMA model
 mod_netmeta <- netmeta(p,reference.group = "Placebo")
 
-## visualize NMA estimates 
+# visualize NMA estimates 
 forest(mod_netmeta,
        overall.hetstat = TRUE, addrows = 0, calcwidth.hetstat = TRUE,
        print.I2 = FALSE, print.tau = TRUE, print.pval.Q = FALSE,
@@ -63,134 +55,69 @@ forest(mod_netmeta,
        #
        drop.reference.group = TRUE,
        #
-       label.left = "Favors vortioxetine",
-       label.right = "Favors other treatments",
+       label.left = "Favors other treatments",
+       label.right = "Favors Placebo",
        #
        header.line = TRUE, spacing = 1.5,
        #
        file = "forest-diabetes-netmeta.pdf")
 
-## get ranking based on P-scores
+# get ranking based on P-scores
 netrank(mod_netmeta,small.values = "desirable")
 
 
 ### RANK TREATMENTS BASED ON THE PROPOSED APPROACH ###
 
-### Define a tcc using 1.20 as the MCID
+### Define a tcc using 1.20 as the SWD
 
 # help(tcc)
 
 ranks <- tcc(mod_netmeta,
              mcid = 1.20,
-             small.values = "desirable"
-)
+             small.values = "desirable")
 
+# Visualize the TCC in terms of the basic parameters ("Treatments vs Placebo")
+# help(forest.tcc)
+forest(ranks,label.right="Favors Placebo",label.left = "Favors other treatments")
 
-## Fit the model and get the ability estimates
-
+# Fit the model and get the ability estimates
 # help(mtrank)
-
 mod_ability <- mtrank(ranks)
 
-## Extract ability estimates
+# Extract ability estimates
 mod_ability$estimates
 
-## Extract probability that each treatment is ranked first
+# Extract probability that each treatment is ranked first
 mod_ability$probabilities
 
-## Vizualize the ability estimates
+# Vizualize the ability estimates
 forest(mod_ability, spacing = 1.5,
        file = "forest-diabetes-mtrank.pdf")
-
-## Calculate pairwise probabilities
-
-#help(paired_pref) 
-
-fitted(mod_ability,treat1 = "ARB",treat2 = "Diuretic",type = "all")
-
-fitted(mod_ability,treat1 = "ARB",treat2 = "ACE",type = "all")
 
 ### RANK TREATMENTS BASED ON THE PReTA approach ###
 
 mod_preta <- alternativenma(mod_netmeta,small.values = "good")
-
-## get ranking results
+# get ranking results
 res_preta <- cbind.data.frame("treat"=row.names(mod_preta$averages),"PReTA_ranking"=mod_preta$averages$Pscoreaverage)
-
 res_preta <- res_preta %>% 
   arrange(desc(PReTA_ranking))
-
 res_preta
 
-### RANK TREATMENTS BASED ON THE ADJUSTED ACCORDING TO THE MCID P-scores ###
+### RANK TREATMENTS BASED ON THE ADJUSTED ACCORDING TO THE SWD P-scores ###
 
 net1 <- list(mod_netmeta)
-
-pscores_mcid <- p_scores(net1,log(1.20),NULL,"H")
-
-res_pscores_mcid <- cbind.data.frame("treat"=names(pscores_mcid),"P_score"=unname(pscores_mcid))
-
-res_pscores_mcid  <- res_pscores_mcid  %>% 
+pscores_SWD <- p_scores(net1,log(1.20),NULL,"H")
+res_pscores_SWD <- cbind.data.frame("treat"=names(pscores_SWD),"P_score"=unname(pscores_SWD))
+res_pscores_SWD  <- res_pscores_SWD  %>% 
   arrange(desc(P_score))
+res_pscores_SWD
 
-res_pscores_mcid
+### RANK TREATMENTS BASED ON THE FREQUENTIST pBV approach ###
 
-### RANK TREATMENTS BASED ON THE BAYESIAN P(best) approach ###
-
-run.data = long2jags(
-  studlab = study,
-  treat = t,
-  event = r,
-  n = n,
-  data = diabetes
-)
-
-drug_names <- levels(as.factor(diabetes$t))
-
-run.list = list(
-  ns = run.data$k,
-  nt = run.data$n,
-  na = run.data$n.k,
-  t = run.data$T,
-  r = run.data$E,
-  n = run.data$N,
-  ref = which(drug_names=="Placebo"),
-  outcome=0 ## small.values are desirable
-)
-
-## Fit the Bayesian model
-
-mod_bayesian = jags(
-  data = run.list,
-  inits = NULL,
-  parameters.to.save = c(
-    "logOR.ref",
-    "tau",
-    "effectiveness",
-    "most.effective"
-  ),
-  n.chains = 2,
-  n.iter = 50000,
-  n.burnin = 10000,
-  DIC = T,
-  model.file = "NMA.model.txt"
-)
-
-## get results
-
-results_Bayesian = as.data.frame(mod_bayesian$BUGSoutput$summary)
-
-results_Bayesian$ind = as.character(rownames(results_Bayesian))
-
-## extract P(best)
-
-p_best = results_Bayesian %>%
-  filter(grepl("most.effective", ind))
-
-p_best <- data.frame("treatment"=drug_names,
-                     "pBV"=p_best[,1])
-
-p_best <- p_best %>% 
-  arrange(desc(pBV))
-
-p_best
+r <- rankogram(mod_netmeta,small.values = "desirable",nsim = 50000)
+pBV <- as.data.frame(r$ranking.matrix.random[,1])
+names(pBV) <- c("pBV")
+pBV <- pBV %>% 
+  arrange(desc(pBV)) %>% 
+  mutate(pBV=round(pBV,digits = 2))
+pBV
